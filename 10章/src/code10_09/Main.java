477623e46269d24f72c4837d03e3dc891d2c86d6
nachi739 <namiki081025@gmail.com>
2021-09-28 16:49:23 +0900

    code10_09

---
+++ 10章/src/code10_09/Main.java
@@ -0,0 +1,13 @@
+package code10_09;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Matango m = new Matango();
+		SuperHero sh = new SuperHero();
+		sh.run();
+		sh.fly();
+		sh.attack(m);
+	}
+
+}
